eba4480a12b0196eb1ea4734c5d4a454e5fb9e9f
JP <joaopaulod.ferreira@hotmail.com>
2026-03-29 16:56:17 -0300

    Atualização da lógica de validação do CPF (11 dígitos)

--- src/main/java/cliente/Fisica.java
+++ src/main/java/cliente/Fisica.java
@@ -12,9 +12,28 @@ public class Fisica extends Pessoa {
     public void cadastra(Scanner tec) {
         System.out.println("----Cadastro de Pessoa Fisica----");
         super.cadastra(tec);
+        
         /*Scanner tec = new Scanner(System.in);*/
-        System.out.println("Digite o cpf");
-        cpf = tec.nextLine();
+        //System.out.println("Digite o cpf");
+        //cpf = tec.nextLine();//04802936940
+        
+        //atualizado 29.03.2026 para validar quantidade de dígitos cpf
+        boolean cpfValido = false;
+        
+        while (!cpfValido) {
+            System.out.println("Digite o CPF (apenas 11 números):");
+            String entrada = tec.nextLine();
+            
+            // Remove espaços ou pontos caso o usuário digite (opcional, mas bom)
+            entrada = entrada.replaceAll("\\D", "");
+            
+            if (entrada.length() != 11) {
+            System.out.println("Erro: O CPF deve ter exatamente 11 dígitos! Você digitou: " + entrada.length());
+            } else {
+            this.cpf = entrada; // Salva na variável da classe
+            cpfValido = true;   // Sai do loop
+            }
+        }
     }
 
     @Override
